63ed68d7b3788e8711e03cdf6d7a1fe001f546e1
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-11-23 11:32:53 +0400

    Добавьте файлы проекта.

---
+++ ModifyString/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ModifyString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            s1.Name = "Jenny";
+
+            Student s2 = new Student();
+            //s2.Name = s1.Name;
+
+            s2.Name = s1.Name.ToUpper();
+            Console.WriteLine("s1 - " + s1.Name + ", s2 - " + s2.Name);
+
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+
+    class Student
+    {
+        public String Name;
+    }
+}
